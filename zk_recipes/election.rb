286535d8c6310514a79980312f71985d56aa8b41
Manas Apte <manasapte@gmail.com>
2014-02-09 21:52:37 -0800

    added more code

--- zk_recipes/election.rb
+++ zk_recipes/election.rb
@@ -3,22 +3,26 @@ module ZkRecipes
     require 'heartbeat'
     HEARTBEAT_PERIOD = 2
     class ElectionCandidate
-      def initialize(zk, app_name, election_ns, handler)
+      def initialize(zk, app_name, data, election_ns, handler)
         @zk          = zk
         @namespace   = app_name
         @election_ns = election_ns
         @handler     = handler
-        @prefix      = get_path([@namespace, @election_ns, "_vote_"]))
+        @prefix      = get_abs_path([@namespace, @election_ns]))
         @heartbeat   = ZkRecipes::Heartbeat.new(@zk, @namespace, HEARTBEAT_PERIOD)
         @started     = false
       end
 
-      def get_path(crumbs)
+      def get_absolute_path(crumbs)
         File.join([""] + crumbs)
       end
 
+      def get_relative_path(prefix, suffix)
+        File.join([prefix, suffix])
+      end
+
       def current_candidates
-        @zk.children(get_path([@zk, @election_ns])).grep(/^_election_/).sort
+        @zk.children(@prefix, :watch => false).grep(/^_vote_/).sort
       end
 
       def clear_parent_subscription
@@ -34,6 +38,9 @@ module ZkRecipes
       def parent_path
         idx = @candidates.index(@path) 
         idx == 0 ? nil : @candidates[idx - 1]
+      end
+
+      def leader_ready 
       end
 
       def handle_parent_event(event)
@@ -62,7 +69,7 @@ module ZkRecipes
         return if @started
         @started = true
         @heartbeat.start
-        @path = @zk.create(:mode => :ephemeral_sequential)
+        @path = @zk.create(get_relative_path(@prefix, "_vote_"), :data => @data, :mode => :ephemeral_sequential)
         vote!
       end
 
@@ -70,6 +77,7 @@ module ZkRecipes
         @candidates = current_candidates 
         if i_the_leader?
           handler.election_won!
+          leader_ready
         else
           handler.election_lost!
           watch_parent
